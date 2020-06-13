01a443928f4784613b5ebe22d9d327537149e6e9
j.e.smit <jarno.e.smit@gmail.com>
2020-06-13 22:37:39 +0200

    Began making one route. Checking the constraints needs to be finished, then a while loop can be integrated to fill the route.

--- Ex2.1-1274850.py
+++ Ex2.1-1274850.py
@@ -9,6 +9,10 @@ Loading the data
 """
 data = pd.read_excel('Data Excercise 2 - EMTE stores - BA 2020-1.xlsx')
 
+"""
+The rounding is a bit ambiguous. Do I round after adding each distance?
+"""
+
 
 #%%
 class Vrp:
@@ -22,6 +26,7 @@ class Vrp:
         self.driving_speed = 90
         self.route_nr = 1
         self.current_time = 9
+        # self.hq_coordinates = (51.607, 5.52805)
 
     def distance_matrix(self):
         for i in self.data['City Nr.']:
@@ -36,48 +41,76 @@ class Vrp:
         self.data['Visit Time'] = visit_times
         return self.data  # TODO: Don't have to return it, but keep temporarily for insight
 
-    def check_constraints(self):
-        pass
+    @staticmethod
+    def distance_per_store(temp_route):
+        cumulative_distances = temp_route['Total Distance in Route (km)'].to_list()
+        cumulative_distances.reverse()
+        cumulative_distances_2 = cumulative_distances[1:]  # Copy every value except the first
+        cumulative_distances.pop(len(cumulative_distances) - 1)  # Pop the last entry
+        distance_per_store = [i - j for i, j in zip(cumulative_distances, cumulative_distances_2)]
+        distance_per_store.reverse()
+        distance_per_store.insert(0, 0)  # Since the first
+        temp_route['Distance from Previous'] = distance_per_store
+        return temp_route
+
+    def check_constraints(self, temp_route):
+        total_driving_time = temp_route['Total Distance in Route (km)'][len(temp_route)-1] / 1.5  # In minutes
+        temp_route = self.distance_per_store(temp_route)
+        print('hierna')  # Doesn't show
+        print(temp_route)
+
+
+        # Continue here --> see
+        """
+        2 constraints:
+        - start & end at HQ
+        - don't work more than 11 hours
+    
+        current_time_after_visit = 09.00 + sum(store_visit_times) + sum(driving times starting from the first store (hq 1 does not count)
+        current_time_after_visit !> closing_time (17.00)
+        start_time = 
+        current_time - start_time !> 11 hours
+        """
+
+
+        total_visit_time = 3
+        time_after_visit = 3
+        return True
 
     def one_route(self):
         current_store = 0
-        if self.route_nr == 1:  # In the first route the HQ is still present
-            dist = pd.DataFrame(self.dm[current_store].to_list())  # Take distances of current store (column)
-            dist.drop(current_store, inplace=True)  # Drop the current store, distance is always 0
-            shortest_distance = dist.min()
-            closest_store = dist.idxmin()  # Returns index (store) of lowest distance
-            store_name = self.data['Name'][closest_store[0]]  # Retrieve name of store from data frame
+        dist = pd.DataFrame(self.dm[current_store].to_list())  # Take distances of current store (column)
+        dist.drop(current_store, inplace=True)  # Drop the current store, distance is always 0
+        if current_store != 0:
+            dist.drop(0, inplace=True)  # HQ is removed, unless it is the first store in the route
+        shortest_distance = dist.min()
+        closest_store = dist.idxmin()  # Returns index (store) of lowest distance
+        store_name = self.data['Name'][closest_store[0]]  # Retrieve name of store from data frame
+        total_route_distance = self.route['Total Distance in Route (km)'][len(self.route) - 1] + shortest_distance[0]
+        temp_route = self.route.copy(deep=True)  # This temporary route is checked for constraints
+        temp_route = temp_route.append({'Route Nr.': self.route_nr, 'City Nr.': closest_store[0],
+                                        'City Name': store_name,
+                                        'Total Distance in Route (km)': total_route_distance},
+                                       ignore_index=True)
+        if self.check_constraints(temp_route):   # Accept the new route and continue
             self.route = self.route.append({'Route Nr.': self.route_nr, 'City Nr.': closest_store[0],
                                             'City Name': store_name,
-                                            'Total Distance in Route (km)': shortest_distance[0]}, ignore_index=True)
-            if check_constraints():
-                # Accept the new route and continue
-                print('ja')
-            else:
-                # Remove the store from the route and end the current route --> add hq to end
-                print('mh')
-            return self.route
-        else:
-            # add hq back to distance matrix
-            dist = pd.DataFrame(self.dm[current_store].to_list())  # Take distances of current store (column)
-            dist.drop(current_store, inplace=True)  # Drop the current store, distance is always 0
-            shortest_distance = dist.min()
-            closest_store = dist.idxmin()  # Returns index (store) of lowest distance
-
-            # Check constraints?
-
-            self.route = self.route.append({'Route Nr.': self.route_nr, 'City Nr.': closest_store,
-                                            'City Name': self.data['Name'][closest_store][0],
-                                            'Total Distance in Route (km)': shortest_distance[0]}, ignore_index=True)
-            return self.route
-
-        # Always start from the headquarters
-        # while True:
-        # dist = pd.DataFrame(route_dm[current_store].to_list())
-        # dist.drop(current_store, inplace=True)  # Drop the current store, distance is always 0
-        shortest_distance = dist.min()
-        closest_store = dist.idxmin()  # Returns index of lowest distance
-        # route_dm.drop(current_store, axis=0, inplace=True)
+                                            'Total Distance in Route (km)': total_route_distance},
+                                           ignore_index=True)  # Add store to route
+            self.dm.drop(closest_store, inplace=True)  # remove the closest store from the distance matrix
+            current_store = closest_store  # Change the current store for next iteration
+        else:  # Add hq to end of route and break from loop
+            last_store = self.route['City Nr.'][len(self.route)-1]
+            distance_to_hq = self.dm[last_store][0]  # Distance from last visited store to hq
+            print(distance_to_hq)
+            total_route_distance = self.route['Total Distance in Route (km)'][len(self.route) - 1] + distance_to_hq
+            self.route = self.route.append({'Route Nr.': self.route_nr, 'City Nr.': 0,
+                                            'City Name': 'EMTE HEADQUARTERS VEGHEL',
+                                            'Total Distance in Route (km)': total_route_distance},
+                                           ignore_index=True)  # Add hq to route
+            # break
+        self.route_nr += 1
+        return self.route
 
 
 #%%
@@ -94,5 +127,7 @@ data = john.add_visit_times()  # TODo: Remember to delete the variable assignmen
 
 #%%
 route = john.one_route()
+print(len(route))
 
+#%%
 
